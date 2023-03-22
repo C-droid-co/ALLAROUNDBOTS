b19fcac39c3cfd190dcb5ae54b959922c9d8bb95
C-droid-co <103402546+C-droid-co@users.noreply.github.com>
2023-03-22 11:24:09 +0100

    Update global_stats.py

--- allr/modules/global_stats.py
+++ allr/modules/global_stats.py
@@ -4,10 +4,10 @@ from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import FloodWait
 
-from wbb import BOT_ID, BOT_NAME, SUDOERS, USERBOT_NAME, app, app2
-from wbb.core.decorators.errors import capture_err
-from wbb.modules import ALL_MODULES
-from wbb.utils.dbfunctions import (
+from allr import BOT_ID, BOT_NAME, SUDOERS, USERBOT_NAME, app, app2
+from allr.core.decorators.errors import capture_err
+from allr.modules import ALL_MODULES
+from allr.utils.dbfunctions import (
     get_blacklist_filters_count,
     get_filters_count,
     get_gbans_count,
@@ -19,8 +19,8 @@ from wbb.utils.dbfunctions import (
     get_warns_count,
     remove_served_chat,
 )
-from wbb.utils.http import get
-from wbb.utils.inlinefuncs import keywords_list
+from allr.utils.http import get
+from allr.utils.inlinefuncs import keywords_list
 
 
 @app.on_message(filters.command("clean_db") & SUDOERS)
